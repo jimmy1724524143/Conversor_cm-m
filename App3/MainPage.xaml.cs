0c3e135b208f19dd62a7414b0b4b1323baa84e24
jimmy1724524143 <133244319+jimmy1724524143@users.noreply.github.com>
2023-07-11 21:29:12 -0500

    Agregar archivos de proyecto.

---
+++ App3/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Image_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btniniciar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new convertir()); 
+        }
+    }
+}
---
+++ App3/convertir.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class convertir : ContentPage
+	{
+
+		double cm;
+		double m;
+
+       
+
+        public convertir ()
+		{
+			InitializeComponent ();
+		
+			
+		
+		}
+		
+		private void Calcular()
+		{
+			cm = Convert.ToDouble(txtcm.Text);
+			m = cm / 100;
+			lblresult.Text = m.ToString() + " m ";
+			
+		}
+		private void validar() 
+		{
+			if (!string.IsNullOrEmpty(txtcm.Text))
+			{
+				Calcular();
+			}
+			else {
+				DisplayAlert("ERROR", "INGRESE UNA CANTIDAD", "OK");
+			} 
+		}
+
+
+
+        private void btnvolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void btncalcular_Clicked(object sender, EventArgs e)
+        {
+			validar();
+        }
+    }
+}
